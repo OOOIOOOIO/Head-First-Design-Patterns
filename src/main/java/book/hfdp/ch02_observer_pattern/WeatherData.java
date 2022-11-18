7706e28a021b9ee13fae8fca4e596ea869c0a346
OOOIOOOIO <polite159@gmail.com>
2022-11-18 14:54:57 +0900

    전략 패턴 끝, 옵저버 패턴 시작

--- src/main/java/book/hfdp/ch02_observer_pattern/WeatherData.java
+++ src/main/java/book/hfdp/ch02_observer_pattern/WeatherData.java
@@ -13,7 +13,7 @@ public class WeatherData {
         반면에 옵저버는 상태를 사용하지만, 반드시 소유할 필요는 없다.
         따라서 옵저버는 여러개가 있을 수 있으며, 주제에서 상태가 바뀌었다는 사실을 알려주길 기다리는 주제에 의존적인 성질을 가지게 된다.(일대다 관계)
      */
-    // oneToMany 관계,
+    // oneToMany 관계
     private List<Observer> observers;
     private float temp;
     private float humidity;
